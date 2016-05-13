9cea8b3e90d6118b9d66c12efbe76a6b4b9181eb
Sanja Maric <anicic@gmail.com>
2016-05-13 12:41:14 -0400

    adding Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/create'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
